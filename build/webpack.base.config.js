868333a288781e06b0a7e37d9edadb3ec7f9d3e8
朱建国#A00336 <1808903692@qq.com>
2022-04-15 08:56:20 +0800

    初始化工程配置

---
+++ build/webpack.base.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+ module.exports = {
+entry: {
+    'app': './src/index.ts'
+},
+output: {
+    filename: '[name].[chunkhash:8].js'
+},
+resolve: {
+    extensions: ['.js', '.tsx', ".ts"]
+},
+module: {
+    rules: [
+        {
+            test: /\.tsx?$/i,
+            use: [{
+                loader: 'ts-loader'
+            }],
+            exclude: /node_modules/
+        }
+    ]
+},
+plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/tpl/index.html'
+    })
+],
+optimization: {
+    splitChunks: {
+        chunks: 'all'
+      }
+    }
+ }
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+let hello:string = "我是小怪兽";
+document.querySelectorAll('.app')[0].innerHTML = hello;
