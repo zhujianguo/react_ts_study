37153cf225ae445363ea1fb5453efa7084fb0098
朱建国#A00336 <1808903692@qq.com>
2022-04-15 15:02:52 +0800

    添加babel插件

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,6 +1,16 @@
+const path = require('path');
 module.exports = {
     devtool: 'eval-cheap-module-source-map',
     devServer: {
         port: 8080
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                use: ["babel-loader?cacheDirectory=true"],
+                include: path.join(__dirname, "../src"),
+            }
+        ]
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-let hello:string = "我是小怪兽";
-document.querySelectorAll('.app')[0].innerHTML = hello;
+// let hello:string = "我是小怪兽";
+// document.querySelectorAll('.app')[0].innerHTML = hello;
+var tem = (str: string) => {
+    document.querySelectorAll('.app')[0].innerHTML = str;
+}
+
+tem('我现在在使用ES6!');
